abea64dd8d67cbb6854b3c56595cc023143ceb3e
doggersen <martin.mbj.johansen@gmail.com>
2021-03-22 18:36:08 +0100

    Add project files.

---
+++ XamarinListViewTut/EmployeeListViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace XamarinListViewTut
+
+{
+    class EmployeeListViewModel
+    {
+        public string[] Employees { get; set; }
+        
+        public EmployeeListViewModel()
+        {
+            
+
+            string[] arrEmployees = new string[] { "batman", "superman", "wonderwoman" };
+            Employees = arrEmployees;
+        }
+
+
+    }
+}
