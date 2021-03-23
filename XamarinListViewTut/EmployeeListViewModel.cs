09999ba509897ebfa88f32c49308a58d084a9df6
doggersen <martin.mbj.johansen@gmail.com>
2021-03-23 18:36:44 +0100

    finished video 9. it now has commands remove, add, and update buttons for the  list.

--- XamarinListViewTut/EmployeeListViewModel.cs
+++ XamarinListViewTut/EmployeeListViewModel.cs
@@ -3,21 +3,58 @@ using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
 using System.Windows.Input;
+using System.Collections.ObjectModel;
 
 namespace XamarinListViewTut
 
 {
     class EmployeeListViewModel
     {
-        public string[] Employees { get; set; }
+        public ICommand AddEmployeeCommand => new Command(AddEmployee);
+        public ICommand RemoveEmployeeCommand => new Command(RemoveEmployee);
+        public ICommand UpdateEmployeeCommand => new Command(UpdateEmployee);
+
+
+        public ObservableCollection<string> Employees { get; set; }
         
+        public string EmployeeName { get; set; }
+
+        public string SelectedEmployee { get; set; }
+
         public EmployeeListViewModel()
         {
-            
-            string[] arrEmployees = new string[] { "batman", "superman", "wonderwoman" };
-            Employees = arrEmployees;
+
+
+
+            //string[] arrEmployees = new string[] { "batman", "superman", "wonderwoman" }; (sådan havde den normalt set ud hvis vi ikke brugte observablecollection)
+            Employees = new ObservableCollection<string>();
+            Employees.Add("batman");
+            Employees.Add("wonderwoman");
+            Employees.Add("superman");
         }
 
+        public void AddEmployee()
+        {
+            Employees.Add(EmployeeName);
+        }
+
+        public void RemoveEmployee()
+        {
+            Employees.Remove(SelectedEmployee);
+        }
+
+        public void UpdateEmployee()
+        {
+
+            int newIndex = Employees.IndexOf(SelectedEmployee);
+            Employees.Remove(SelectedEmployee);
+
+            Employees.Add(EmployeeName);
+            int oldIndex = Employees.IndexOf(EmployeeName);
+
+            Employees.Move(oldIndex, newIndex); 
+
+        }
 
     }
 }
